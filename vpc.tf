52030ec4518dc416c45cc27c908b94f90ed84d8b
Lokraj25 <Lokrajthakur25@gmail.com>
2025-04-12 11:55:13 +0530

    create public subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "ibm-vpc" {
     Name = "ibm"
   }
 }
+
+# subnet
+resource "aws_subnet" "ibm-web-sn" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone ="ap-northeast-1a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "ibm-web-sn"
+  }
+}
