f2905dfec53fd3284111b1416960c8ed567a62c8
Lokraj25 <Lokrajthakur25@gmail.com>
2025-04-13 17:16:36 +0530

    aws provider -runtime authentication

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 # EC2 server
 
-resource "aws_instance" "ibm_web_server" {
+resource "aws_instance" "ecomm_web_server" {
   ami           = "ami-0f415cc2783de6675"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ibm-web-sn.id
+  subnet_id     = aws_subnet.ecomm-web-sn.id
   key_name      = "aws830"
-  vpc_security_group_ids = [aws_security_group.ibm_web_sg.id]
-
+  vpc_security_group_ids = [aws_security_group.ecomm_web_sg.id]
+  user_data = file("ecomm.sh")
 
 
   tags = {
-    Name = "ibm_web_server"
+    Name = "ecomm_web_server"
   }
 }
