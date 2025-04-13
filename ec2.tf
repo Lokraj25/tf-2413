9fe8cd3d0ff0eb58350e374e22da92d7c379f495
Lokraj25 <Lokrajthakur25@gmail.com>
2025-04-13 16:45:39 +0530

    edited ami

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # EC2 server
 
-resource "aws_instance" "ibm-web-server" {
-  ami           = ami-0f415cc2783de6675
+resource "aws_instance" "ibm_web_server" {
+  ami           = "ami-0f415cc2783de6675"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.ibm-web-sn.id
   key_name      = "aws830"
